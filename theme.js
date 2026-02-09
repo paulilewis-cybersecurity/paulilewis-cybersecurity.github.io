7076ab32771841446118443cc615de2fdf03945e
paulilewis-cybersecurity <107197501+paulilewis-cybersecurity@users.noreply.github.com>
2026-02-09 08:52:34 +0000

    Add files via upload

--- theme.js
+++ theme.js
@@ -1,3 +1,2 @@
 
-(function(){ const t=localStorage.getItem('theme'); if(t==='light') document.body.classList.add('light');})();
-function toggleTheme(){ document.body.classList.toggle('light'); localStorage.setItem('theme', document.body.classList.contains('light')?'light':'dark'); }
+function toggleMode(){document.body.classList.toggle('light');}
